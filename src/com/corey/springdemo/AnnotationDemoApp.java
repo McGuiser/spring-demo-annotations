26ae6cd0eadf08a021d0e760fd796b6a45086b9d
Corey Castillo <mcguiser@users.noreply.github.com>
2018-07-17 16:23:25 -0500

    Constructor Injection demo
    Define the dependency interface and class,
    Create a constructor in the class for injections,
    Configure the dependency injection with Autowired Annotation.

--- src/com/corey/springdemo/AnnotationDemoApp.java
+++ src/com/corey/springdemo/AnnotationDemoApp.java
@@ -16,6 +16,9 @@ public class AnnotationDemoApp {
 		// Call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
+		// Call method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
+		
 		// Close the context
 		context.close();
 
